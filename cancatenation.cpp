0b6364e86bdc0358080288121cb6a78db50604e5
AyushaRajput <ayusha.rajput24@sanjivani.edu.in>
2024-08-20 21:26:52 +0530

    Add files via upload
    This C++ program demonstrates string concatenation, where two strings are combined to form a new string. It defines two string variables, "str1" and "str2", with the values "Ayusha" and "Rajput", respectively. These two strings are then concatenated using the "+" operator and stored in a third string variable, str. The program then outputs the combined string "AyushaRajput" to the screen. Finally, the program ends by returning "0", indicating successful execution.

---
+++ cancatenation.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string str1 = "Ayusha";
+string str2 = "Rajput";
+string str = str1 + str2;
+cout << str;
+return 0;
+}
